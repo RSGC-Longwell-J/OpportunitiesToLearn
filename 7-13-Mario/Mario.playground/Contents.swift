e0a181490414d9b7c7df7ea5370a2e12a2fe0c97
Longwell <Longwell@longwell.local>
2016-12-05 08:47:44 -0500

    My Mario code

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -11,3 +11,49 @@
  */
 
 // Add your code below
+func levelCost(heights: [Int], maxJump: Int) -> Int
+
+var previousheight = 0
+
+var energy = 0
+
+
+
+for i in heights{
+    var jumpheight = previousheight - i
+    
+    if previousheight == 0 {
+        
+        previousheight = i
+    }
+    
+} else {
+    
+    
+    
+    if jumpheight < 0{
+        jumpheight = -jumpheight
+    }
+    
+    if jumpHeight > maxJump {
+        return -1
+    }
+    
+    if jumpHeight == 0 {
+        totalEnergy += 1
+    } else {
+        Energy += 2 * jumpHeight
+    }
+    
+    previousheight = i
+}
+}
+
+return totalEnergy
+}
+
+
+
+
+
+
